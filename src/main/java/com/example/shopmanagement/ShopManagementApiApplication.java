529b99fb7402f4e51516f951e73475388a93849c
markReo-code <youyilangtutian@gmail.com>
2026-07-21 11:39:52 +0900

    Initial commit

---
+++ src/main/java/com/example/shopmanagement/ShopManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopManagementApiApplication.class, args);
+	}
+
+}
